e7b4b942e30381f20418fbb11eb6c8cc4b05e5e0
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-05-05 09:57:30 +0100

    Update .gitignore and add basic split view

--- CoffeeTime.Main/ViewModels/MainWindowViewModel.cs
+++ CoffeeTime.Main/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,26 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CoffeeTime.Main.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private string headerText;
+        private string _headerText;
+
+        [ObservableProperty] private bool _isPaneOpen;
 
         public MainWindowViewModel()
         {
             HeaderText = "This is header text.";
+            IsPaneOpen = true;
+        }
+
+        [RelayCommand]
+        private void TogglePane()
+        {
+            IsPaneOpen = !IsPaneOpen;
         }
     }
 }
