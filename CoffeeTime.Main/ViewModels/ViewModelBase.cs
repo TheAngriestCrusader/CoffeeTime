f53bba054758f05c85dfefc2b5171687c83dc452
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-05-05 07:32:17 +0100

    Add CoffeeTime.Main project

---
+++ CoffeeTime.Main/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CoffeeTime.Main.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ CoffeeTime.Main/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CoffeeTime.Main.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
