3eaeaf5d51eca57c1c8cacda18edf7eb06cc41be
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-05-05 12:00:49 +0100

    Clean up and annotate ServiceConfiguration

--- CoffeeTime.Main/ServiceConfiguration.cs
+++ CoffeeTime.Main/ServiceConfiguration.cs
@@ -1,6 +1,7 @@
 using CoffeeTime.Main.ViewModels;
 using System;
 using CoffeeTime.Main.States;
+using CoffeeTime.Main.Views;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoffeeTime.Main
@@ -10,8 +11,14 @@ namespace CoffeeTime.Main
         public static IServiceProvider ConfigureServices()
         {
             ServiceCollection services = new();
+            
+            // States
             services.AddSingleton<HeaderState>();
+            services.AddSingleton<MainDisplayState>();
+            
+            // View Models
             services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<SplashScreenViewModel>();
 
             return services.BuildServiceProvider();
         }
