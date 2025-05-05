f4c81d0410d6c66ce2d6cfcd254d069ddb108016
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-05-05 15:27:23 +0100

    Create Converter class
    The Converter class contains public static functions that can be used anywhere in the program to convert from one resource to another. The first example implemented is to convert avares paths to usable bitmaps.

---
+++ CoffeeTime/Utilities/Converter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace CoffeeTime.Utilities;
+
+public class Converter
+{
+    public static Bitmap AvaresToBitmap(string avares)
+    {
+        return new Bitmap(AssetLoader.Open(new Uri(avares)));
+    }
+}
