3e66c50b4b338618f3e45d7d8131e5d2289111c4
TheAngriestCrusader <theangriestcrusader@gmail.com>
2025-05-15 09:55:17 +0100

    Add UI for Path Watcher and initial ViewModel setup
    Introduced a new UI layout for managing path watchers, including a text box for adding paths and a scrollable list for displaying them. Added `PathWatcherViewModel` properties and commands for handling the path watcher configuration. This sets the foundation for future functionality.

--- CoffeeTime/Modules/PathWatcher/ViewModels/PathWatcherViewModel.cs
+++ CoffeeTime/Modules/PathWatcher/ViewModels/PathWatcherViewModel.cs
@@ -1,8 +1,23 @@
-﻿using CoffeeTime.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CoffeeTime.States;
+using CoffeeTime.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CoffeeTime.Modules.PathWatcher.ViewModels;
 
 public partial class PathWatcherViewModel : ViewModelBase
 {
+    [ObservableProperty] private ObservableCollection<string> _pathWatchers = [];
     
+    public PathWatcherViewModel(HeaderState header)
+    {
+        header.Text = "Path Watcher";
+    }
+
+    [RelayCommand]
+    public void ConfigurePathWatcher()
+    {
+        
+    }
 }
